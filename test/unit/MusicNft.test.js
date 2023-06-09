c2b99d5b3a5d516a1a8c8cd61f75f45c69df76ec
palmcivet7 <palmcivet@proton.me>
2023-06-09 17:08:26 +0100

    write test for burn and withdraw functions

--- test/unit/MusicNft.test.js
+++ test/unit/MusicNft.test.js
@@ -44,22 +44,58 @@ describe("MusicNft", function () {
             expect(afterTokenId).to.equal(beforeTokenId + 1)
         })
 
-        // it("sets the correct token URI", async function () {
-        //     const tokenUri = "tokenURI"
-        //     await musicNft.connect(deployer).mintToken(deployer.address, tokenUri, {
-        //         value: ethers.utils.parseEther("1"), // sending 1 FTM
-        //     })
-        //     const tokenId = await musicNft.getCurrentTokenId() // Get the latest token's ID
-        //     const retrievedTokenUri = await musicNft.tokenURI(tokenId)
-        //     expect(retrievedTokenUri).to.equal(tokenUri)
-        // })
-        // it("sets the correct minter", async function () {
-        //     await musicNft.connect(deployer).mintToken(deployer.address, "https://tokenuri.com", {
-        //         value: ethers.utils.parseEther("1"), // sending 1 FTM
-        //     })
-        //     const tokenId = await musicNft.getCurrentTokenId()
-        //     const minter = await musicNft._minters(tokenId)
-        //     expect(minter).to.equal(deployer.address)
-        // })
+        it("emits an event and sets the correct token URI and minter", async function () {
+            const tokenUri = "tokenURI"
+            const mintTx = await musicNft.connect(deployer).mintToken(deployer.address, tokenUri, {
+                value: ethers.utils.parseEther("1"), // sending 1 FTM
+            })
+            expect(mintTx).to.emit(musicNft, "Minted").withArgs(deployer.address, 1, tokenUri)
+            const receipt = await mintTx.wait()
+            const event = receipt.events.find((e) => e.event === "Minted")
+            const tokenId = event.args[1]
+            const retrievedTokenUri = await musicNft.tokenURI(tokenId)
+            expect(retrievedTokenUri).to.equal(tokenUri)
+            const minter = await musicNft._minters(tokenId)
+            expect(minter).to.equal(deployer.address)
+        })
+    })
+
+    describe("burn function", () => {
+        it("does not allow a non-approved address (ie minter if they are still owner) to burn the token", async function () {
+            await musicNft.connect(deployer).mintToken(deployer.address, "tokenURI", {
+                value: ethers.utils.parseEther("1"), // sending 1 FTM
+            })
+            const currentTokenId = await musicNft.getCurrentTokenId()
+            const tokenId = currentTokenId.sub(1) // Get the most recently minted token
+            await expect(musicNft.connect(accounts[1]).burn(tokenId)).to.be.revertedWith(
+                "MusicNft__CanOnlyBeBurnedIfOwnedByMinter"
+            )
+        })
+        it("burns the token successfully, reducing owner's balance", async function () {
+            // Mint a new token
+            await musicNft.connect(deployer).mintToken(deployer.address, "tokenURI", {
+                value: ethers.utils.parseEther("1"), // sending 1 FTM
+            })
+            const initialOwnerBalance = await musicNft.balanceOf(deployer.address)
+            const tokenIdToBurn = (await musicNft.getCurrentTokenId()).toNumber() - 1
+            await musicNft.connect(deployer).burn(tokenIdToBurn)
+            const finalOwnerBalance = await musicNft.balanceOf(deployer.address)
+            expect(finalOwnerBalance).to.equal(initialOwnerBalance - 1)
+        })
+    })
+
+    describe("withdraw function", () => {
+        it("reverts if there is no balance to withdraw", async function () {
+            await expect(musicNft.withdraw()).to.be.revertedWith("MusicNft__NothingToWithdraw")
+        })
+        it("transfers the contract's balance to the owner", async function () {
+            await musicNft.connect(deployer).mintToken(deployer.address, "tokenURI", {
+                value: ethers.utils.parseEther("1"), // sending 1 FTM
+            })
+            const initialBalance = await ethers.provider.getBalance(deployer.address)
+            await musicNft.connect(deployer).withdraw()
+            const finalBalance = await ethers.provider.getBalance(deployer.address)
+            expect(finalBalance).to.be.gt(initialBalance)
+        })
     })
 })
