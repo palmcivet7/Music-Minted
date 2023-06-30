87576887a2d9116a33b172834179ca3b7a4b9d61
palmcivet7 <palmcivet@proton.me>
2023-06-30 13:27:52 +0100

    update intro paragraph

--- music-minted-frontend/components/Paragraph.js
+++ music-minted-frontend/components/Paragraph.js
@@ -4,11 +4,11 @@ export default function Paragraph() {
     return (
         <div className={styles.paragraph}>
             <p>
-                Music Minted is an application for musicians to mint their music into nfts. It costs
-                $1 per mint, and uses Chainlink price feeds to ensure that. When files are
-                submitted, the audio and cover art are uploaded to AWS S3, and are then included in
-                a json file along with the other submitted info. This metadata json file gets passed
-                to the nft contract as the token uri.
+                Music Minted is a Fantom-based application for musicians to mint their music into
+                NFTs. It costs $1 per mint, and uses Chainlink price feeds to ensure that. When
+                files are submitted, the audio and cover art are uploaded to AWS S3, and then
+                included in a JSON file along with the other submitted info. This metadata JSON file
+                gets passed to the Music Minted NFT contract as the token URI.
             </p>
         </div>
     )
