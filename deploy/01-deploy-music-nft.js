e97a5796b8e6c05a6c24dec3f6fc81142ffcfc7e
palmcivet7 <palmcivet@proton.me>
2023-06-30 12:44:00 +0100

    include constructor arguments in deploy script

--- deploy/01-deploy-music-nft.js
+++ deploy/01-deploy-music-nft.js
@@ -4,8 +4,14 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy } = deployments
     const { deployer } = await getNamedAccounts()
 
+    const priceFeedAddress = "0xf4766552d15ae4d256ad41b6cf2933482b0680dc" // replace with price feed address
+    // MockV3Aggregator = 0x5FbDB2315678afecb367f032d93F642f64180aa3
+    // FTM/USD FTM TESTNET = 0xe04676B9A9A2973BCb0D1478b5E1E9098BBB7f3D
+    // FTM/USD FTM MAINNET = 0xf4766552d15ae4d256ad41b6cf2933482b0680dc
+
     const musicNFT = await deploy("MusicNft", {
         from: deployer,
+        args: [priceFeedAddress],
         log: true,
     })
 
